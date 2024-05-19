6914716b2064df352f4731bacbba935dddc6a459
QuiLion7 <quilbrub@gmail.com>
2024-05-19 16:05:24 -0300

    feat: add redirect logic and authentication components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,39 @@
+"use client";
 import { ModeToggle } from "@/components/mode-toggle";
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+  useUser,
+} from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+
+  const { user, isLoaded } = useUser();
+
+  useEffect(() => {
+    if (user) {
+      router.push("dashboard");
+    } else {
+      isLoaded && router.push("courses");
+    }
+  }, [user]);
+
   return (
-    <main className="relative">
-      <div className="absolute">
+    <main className="relative flex min-h-screen w-full flex-col items-center justify-center">
+      {/* <div className="absolute">
         <ModeToggle />
-      </div>
+      </div> */}
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </main>
   );
 }
