5f3183f4099aff71412423f7168caead080b2fa6
QuiLion7 <quilbrub@gmail.com>
2024-05-19 16:42:56 -0300

    feat: add check for free or paid course in CourseEnrollSection

--- src/app/(router)/course-preview/components/CourseEnrollSection.tsx
+++ src/app/(router)/course-preview/components/CourseEnrollSection.tsx
@@ -1,24 +1,48 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { courseLists } from "@/utils/constants";
+import { useUser } from "@clerk/nextjs";
+import Link from "next/link";
 import React from "react";
 
-const CourseEnrollSection = () => {
+interface CourseEnrollSectionProps {
+  courseId: string;
+}
+
+const CourseEnrollSection: React.FC<CourseEnrollSectionProps> = ({
+  courseId,
+}) => {
+  const course = courseLists.find((course) => course.id === courseId);
+
   const membership = false;
+  const { user } = useUser();
+
   return (
     <div className="flex flex-col justify-center rounded-xl bg-primary p-3 text-center">
       <h2 className="mb-2 text-xl font-bold text-primary-foreground">
         Inscreva-se no curso
       </h2>
-      {membership && (
+      {user && (membership || course?.free) ? (
         <div className="flex flex-col justify-center gap-3">
           <h2 className="font-light text-primary-foreground">
             Inscreva-se agora para começar a aprender e construir o projeto
           </h2>
-          <Button className="uppercase transition-all hover:animate-pulse hover:bg-secondary-foreground hover:text-primary">
+          <Button className="bg-secondary-foreground uppercase hover:bg-pink-600 hover:text-primary-foreground">
             Inscreva-se agora
           </Button>
         </div>
-      )}
-      {!membership && (
+      ) : !user ? (
+        <div className="flex flex-col justify-center gap-3">
+          <h2 className="font-light text-primary-foreground">
+            Inscreva-se agora para começar a aprender e construir o projeto
+          </h2>
+          <Link href={"/sign-in"}>
+            <Button className="bg-secondary-foreground uppercase hover:bg-pink-600 hover:text-primary-foreground">
+              Inscreva-se agora
+            </Button>
+          </Link>
+        </div>
+      ) : (
         <div className="flex flex-col justify-center gap-3">
           <h2 className="font-light text-primary-foreground">
             Compre uma assinatura mensal e tenha acesso a todos os cursos
