a0a5b8ab377411e083f770567428d3fb4c5ae269
QuiLion7 <quilbrub@gmail.com>
2024-05-19 16:11:13 -0300

    feat: create CourseContentSection component

---
+++ src/app/(router)/course-preview/components/CourseContentSection.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { courseLists } from "@/utils/constants";
+import { Lock, Play } from "lucide-react";
+import React, { useState } from "react";
+
+interface CourseContentSectionProps {
+  courseId: string;
+}
+
+const CourseContentSection: React.FC<CourseContentSectionProps> = ({
+  courseId,
+}) => {
+  const course = courseLists.find((course) => course.id === courseId);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  return (
+    <div className="flex w-full flex-col justify-center gap-3 rounded-xl bg-primary-foreground p-3">
+      <h2>Conteúdo</h2>
+      {course?.chapter.map((item, index) => (
+        <div
+          key={index}
+          className="rounded-xl hover:bg-secondary hover:text-primary"
+        >
+          <h2
+            className={`flex cursor-pointer items-center justify-between rounded-xl border px-3 py-2 text-base ${activeIndex == index && "bg-primary text-primary-foreground"}`}
+          >
+            {index + 1}. {item.name}
+            {activeIndex == index ? (
+              <Play className="h-4 w-4" />
+            ) : (
+              <Lock className="h-4 w-4" />
+            )}
+          </h2>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CourseContentSection;
