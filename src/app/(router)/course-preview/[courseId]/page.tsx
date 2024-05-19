b2ec2c9460f9a0fae69b0389e3218327e36fdce2
QuiLion7 <quilbrub@gmail.com>
2024-05-19 16:10:51 -0300

    feat: create CoursePreview page with related components

---
+++ src/app/(router)/course-preview/[courseId]/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import CourseVideoDescription from "../components/CourseVideoDescription";
+import CourseEnrollSection from "../components/CourseEnrollSection";
+import CourseContentSection from "../components/CourseContentSection";
+
+const CoursePreview = ({ params }: { params: { courseId: string } }) => {
+  return (
+    <div className="grid grid-cols-1 gap-3 p-4 md:grid-cols-3">
+      <div className="col-span-2 rounded-xl bg-primary-foreground p-4">
+        <CourseVideoDescription courseId={params.courseId} />
+      </div>
+
+      <div className="flex flex-col gap-4">
+        <CourseEnrollSection />
+        <CourseContentSection courseId={params.courseId} />
+      </div>
+    </div>
+  );
+};
+
+export default CoursePreview;
