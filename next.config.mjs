5e6c329c39486a9f896558d1ab0d0f009534414b
QuiLion7 <quilbrub@gmail.com>
2024-05-19 16:00:17 -0300

    chore: allow remote images from specified domains in next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        pathname: "**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
