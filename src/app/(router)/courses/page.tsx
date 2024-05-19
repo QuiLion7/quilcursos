21d87c3bf32550403e91d7d516a31876200406ba
QuiLion7 <quilbrub@gmail.com>
2024-05-19 16:15:04 -0300

    feat: add CourseList and SideBanners components to Courses page

--- src/app/(router)/courses/page.tsx
+++ src/app/(router)/courses/page.tsx
@@ -1,12 +1,17 @@
+import CourseList from "./components/CourseList";
+import SideBanners from "./components/SideBanners";
 import WelcomeBanner from "./components/WelcomeBanner";
 
 export default function Courses() {
   return (
-    <div className="grid grid-cols-1 p-4 md:grid-cols-3">
-      <div className="col-span-2">
+    <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-4">
+      <div className="col-span-3 flex flex-col gap-4">
         <WelcomeBanner />
+        <CourseList />
       </div>
-      <div>lado direito</div>
+      <div className="flex items-start justify-center rounded-xl bg-primary-foreground p-4">
+        <SideBanners />
+      </div>
     </div>
   );
 }
