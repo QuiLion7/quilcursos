9eee6a84009d875b36d997d5d633233e9f56be9a
QuiLion7 <quilbrub@gmail.com>
2024-05-19 23:31:26 -0300

    refactor: improve responsiveness of VideoPlayer and remove unused function and state

--- src/app/(router)/course-preview/components/VideoPlayer.tsx
+++ src/app/(router)/course-preview/components/VideoPlayer.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Skeleton } from "@/components/ui/skeleton";
 import React, { useState } from "react";
 import ReactPlayer from "react-player";
 
@@ -7,17 +8,9 @@ interface VideoPlayerProps {
 }
 
 const VideoPlayer: React.FC<VideoPlayerProps> = ({ videoUrl }) => {
-  // const [duration, setDuration] = useState(0);
-
   return (
-    <div className="flex h-[360px] w-full flex-col items-center">
-      <ReactPlayer
-        width="100%"
-        height="100%"
-        url={videoUrl}
-        controls={true}
-        // onDuration={(duration: number) => setDuration(duration)}
-      />
+    <div className="flex aspect-video w-full flex-col items-center bg-background">
+      <ReactPlayer width="100%" height="100%" url={videoUrl} controls={false} />
     </div>
   );
 };
