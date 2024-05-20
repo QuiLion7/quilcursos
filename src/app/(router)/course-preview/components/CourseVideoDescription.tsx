2ba1ae1dc3bca0f867c6841ef02747002572a080
QuiLion7 <quilbrub@gmail.com>
2024-05-19 23:30:20 -0300

    feat: add Collapsible to toggle video description in CourseVideoDescription

--- src/app/(router)/course-preview/components/CourseVideoDescription.tsx
+++ src/app/(router)/course-preview/components/CourseVideoDescription.tsx
@@ -1,8 +1,16 @@
 "use client";
+
 import { courseLists } from "@/utils/constants";
 import React, { useEffect, useState } from "react";
 import VideoPlayer from "./VideoPlayer";
-import Markdown from "react-markdown";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import { ChevronDown, ChevronUp } from "lucide-react";
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@/components/ui/label";
 
 interface CourseVideoDescriptionProps {
   courseId: string;
@@ -12,6 +20,7 @@ const CourseVideoDescription: React.FC<CourseVideoDescriptionProps> = ({
   courseId,
 }) => {
   const course = courseLists.find((course) => course.id === courseId);
+  const [openCollapsible, setOpenCollapsible] = useState(false);
 
   const [reloadingPage, setReloadingPage] = useState(false);
 
@@ -24,7 +33,7 @@ const CourseVideoDescription: React.FC<CourseVideoDescriptionProps> = ({
       {!course && <div>Curso não encontrado.</div>}
 
       {course && (
-        <div className="flex flex-col justify-start gap-2">
+        <div className="flex flex-col justify-start gap-4">
           <h2 className="text-2xl font-semibold">{course.name}</h2>
           <h2 className="text-xl">{course.author}</h2>
           <div className="h-full w-full">
@@ -32,10 +41,23 @@ const CourseVideoDescription: React.FC<CourseVideoDescriptionProps> = ({
               <VideoPlayer videoUrl={course.chapter[0].video.url} />
             )}
           </div>
-          <h2>Sobre este Curso</h2>
-          <Markdown className="text-lg font-light leading-7">
-            {course.description}
-          </Markdown>
+
+          <Collapsible open={openCollapsible} onOpenChange={setOpenCollapsible}>
+            <CollapsibleTrigger className="flex items-center justify-center gap-2 text-lg font-bold uppercase">
+              Sobre este Curso
+              {openCollapsible ? <ChevronUp /> : <ChevronDown />}
+            </CollapsibleTrigger>
+            <CollapsibleContent className="mb-2">
+              {course.description}
+            </CollapsibleContent>
+          </Collapsible>
+
+          <div className="grid w-full gap-1.5">
+            <Label htmlFor="message" className="text-base uppercase">
+              faça a sua anotação
+            </Label>
+            <Textarea placeholder="Digite sua anotação aqui." id="message" />
+          </div>
         </div>
       )}
     </div>
