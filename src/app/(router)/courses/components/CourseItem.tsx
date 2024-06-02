edc8fbb1bb2e5b1b8503d83f72716f8e3f06d0cb
QuiLion7 <quilbrub@gmail.com>
2024-06-02 11:38:11 -0300

    style: adjust font size and grid layout for responsiveness

--- src/app/(router)/courses/components/CourseItem.tsx
+++ src/app/(router)/courses/components/CourseItem.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const CourseItem = () => {
   return (
-    <div className="mt-4 grid grid-cols-2 gap-4 lg:grid-cols-3">
+    <div className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
       {courseLists.map((course, index) => (
         <Link key={index} href={`/course-preview/${course.id}`}>
           <div className="flex h-full cursor-pointer flex-col gap-1 rounded-xl border hover:shadow-md hover:shadow-primary">
@@ -17,8 +17,12 @@ const CourseItem = () => {
               className="w-full rounded-t-xl"
             />
             <div className="flex flex-col  gap-1 p-2">
-              <h2 className="text-xl font-semibold">{course.name}</h2>
-              <h2 className="text-secondary-foreground">{course.author}</h2>
+              <h2 className="text-base font-semibold sm:text-xl">
+                {course.name}
+              </h2>
+              <h2 className="text-sm text-secondary-foreground sm:text-base">
+                {course.author}
+              </h2>
               {course?.chapter?.length == 0 ? (
                 <div className="flex gap-2">
                   <Image
@@ -30,12 +34,12 @@ const CourseItem = () => {
                     height={20}
                     style={{ width: "auto", height: "auto" }}
                   />
-                  <h2 className="text-lg text-secondary-foreground">
+                  <h2 className="text-xs text-secondary-foreground sm:text-lg">
                     Assistir Aulas
                   </h2>
                 </div>
               ) : (
-                <div className="flex  gap-2">
+                <div className="flex items-center justify-start gap-2">
                   <Image
                     src="/youtube.png"
                     alt="logo youtube"
@@ -45,13 +49,13 @@ const CourseItem = () => {
                     height={20}
                     style={{ width: "auto", height: "auto" }}
                   />
-                  <h2 className="text-lg text-secondary-foreground">
+                  <h2 className="text-xs text-secondary-foreground sm:text-lg">
                     Assistir no Youtube
                   </h2>
                 </div>
               )}
 
-              <div className="font-medium text-secondary-foreground ">
+              <div className=" text-sm font-medium text-secondary-foreground ">
                 {course.free ? "Gratuito" : "Pago"}
               </div>
             </div>
