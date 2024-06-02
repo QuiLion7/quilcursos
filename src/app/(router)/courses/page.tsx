664f44f6a035ad3f7d1d88d8bc7fab6f5949e371
QuiLion7 <quilbrub@gmail.com>
2024-06-02 11:38:47 -0300

    style: add w-full class for improved layout

--- src/app/(router)/courses/page.tsx
+++ src/app/(router)/courses/page.tsx
@@ -4,12 +4,12 @@ import WelcomeBanner from "./components/WelcomeBanner";
 
 export default function Courses() {
   return (
-    <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-4">
+    <div className="grid w-full grid-cols-1 gap-4 p-4 md:grid-cols-4">
       <div className="col-span-3 flex flex-col gap-4">
         <WelcomeBanner />
         <CourseList />
       </div>
-      <div className="flex items-start justify-center rounded-xl bg-background p-4">
+      <div className="flex w-full items-start justify-center rounded-xl bg-background p-4">
         <SideBanners />
       </div>
     </div>
