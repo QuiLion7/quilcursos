0ce30201c04f439147ba1be34fffff20632b79aa
QuiLion7 <quilbrub@gmail.com>
2024-06-02 11:41:41 -0300

    refactor: update home page with new design and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,19 +30,34 @@ export default function Home() {
       <div className="absolute left-1 top-0 p-4">
         <ModeToggle />
       </div>
-      <div>
-        <SignedOut>
-          <Button>
-            <SignInButton />
-          </Button>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </div>
-      <Link href={"/courses"}>
-        <Button>Courses</Button>
-      </Link>
+      <section className="w-full text-primary-foreground">
+        <div className="w-full px-4 py-32 lg:flex lg:h-screen lg:items-center">
+          <div className="flex w-full flex-col items-center justify-center gap-4 text-center">
+            <h1 className="flex flex-col items-center justify-center gap-2 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
+              Invista em quem mais importa.
+              <span className="sm:block"> Invista em você. </span>
+            </h1>
+
+            <p className="text-secondary-foreground sm:text-xl/relaxed">
+              Página em Construção
+            </p>
+
+            <div className="flex w-full items-center justify-center gap-4 ">
+              <SignedOut>
+                <Button>
+                  <SignInButton />
+                </Button>
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+              <Link href={"/courses"}>
+                <Button>Courses</Button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
